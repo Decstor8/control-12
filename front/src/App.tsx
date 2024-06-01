3e5f7e22e1a5b713731d7316a050500feb9f02c7
Tkachev Maksim <maks93makula@gmail.com>
2024-06-01 14:02:13 +0600

    Создана директория components с Логином, Регистрацией и баром Теперь можно зарегистрироваться и логиниться, создан axiosApi

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,23 @@
+import './App.css';
+import {Route, Routes } from 'react-router-dom';
+import AppToolbar from './components/Toolbar';
+import Login from './features/Users/Login';
+import Register from './features/Users/Register';
 
 function App() {
 
-  return (
-    <>
-    </>
-  );
+    return (
+        <>
+            <header>
+                <AppToolbar />
+            </header>
+            <Routes>
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="*" element={<h2>not found!</h2>} />
+            </Routes>
+        </>
+    )
 }
 
 export default App;
---
+++ front/src/types.d.ts
@@ -0,0 +1,41 @@
+export interface RegisterMutation {
+    email: string;
+    password: string;
+    displayName: string;
+    image: string | null;
+}
+
+export interface LoginMutation {
+    email: string;
+    password: string;
+}
+
+export interface UserTypes {
+    _id: string;
+    email: string;
+    token: string;
+    role: string;
+    displayName: string;
+    avatar: string | null;
+}
+
+export interface MainError {
+    error: string;
+}
+
+export interface ValidationError {
+    errors: {
+        [key: string]: {
+            name: string;
+            message: string;
+        },
+    },
+    message: string;
+    name: string;
+    _message: string;
+}
+
+export interface RegisterResponse {
+    message: string;
+    user: UserTypes;
+}
